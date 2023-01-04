dadafe8e18872676ab2c5d258cd8043fe3268352
aabha05 <aabharajpal0805@gmail.com>
2023-01-04 13:29:34 +0530

    huh

--- sketch.js
+++ sketch.js
@@ -1,139 +1,73 @@
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var ball,ground;
-var stand1,stand2;
-var slingShot;
-var rock_img;
+var backgroundImg;
+var hour;
 
-function preload(){
-  rock_img=loadImage("rock.png");
+var bg = "sunrise.png";
+
+function preload() {
+    getBackgroundImg();
+    
 }
-function setup() {
 
-  createCanvas(900,400);
-  engine = Engine.create();
-  world = engine.world;
-  Engine.run(engine);
-  
-  ground = new Ground();
-  stand1 = new Stand(390,300,250,10);
-  stand2 = new Stand(700,200,200,10);
- 
-  //level one
-  block1 = new Block(300,275,30,40);
-  block2 = new Block(330,275,30,40);
-  block3 = new Block(360,275,30,40);
-  block4 = new Block(390,275,30,40);
-  block5 = new Block(420,275,30,40);
-  block6 = new Block(450,275,30,40);
-  block7 = new Block(480,275,30,40);
-  //level two
-  block8 = new Block(330,235,30,40);
-  block9 = new Block(360,235,30,40);
-  block10 = new Block(390,235,30,40);
-  block11 = new Block(420,235,30,40);
-  block12 = new Block(450,235,30,40);
-  //level three
-  block13 = new Block(360,195,30,40);
-  block14 = new Block(390,195,30,40);
-  block15 = new Block(420,195,30,40);
-  //top
-  block16 = new Block(390,155,30,40);
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
 
-  //set 2 for second stand
-  //level one
-  blocks1 = new Block(640,175,30,40);
-  blocks2 = new Block(670,175,30,40);
-  blocks3 = new Block(700,175,30,40);
-  blocks4 = new Block(730,175,30,40);
-  blocks5 = new Block(760,175,30,40);
-  //level two
-  blocks6 = new Block(670,135,30,40);
-  blocks7 = new Block(700,135,30,40);
-  blocks8 = new Block(730,135,30,40);
-  //top
-  blocks9 = new Block(700,95,30,40);
+}
+
+function draw(){
+    if(backgroundImg)
+        background(backgroundImg);
+
+    Engine.update(engine);
+
+    fill("black");
+    textSize(30);
+    
+    if(hour>=12){
+        text("Time : "+ hour%12 + " PM", 50,100);
+    }else if(hour==0){
+        text("Time : 12 AM",100,100);
+    }else{
+        text("Time : "+ hour%12 + " AM", 50,100);
+    }
+
+}
+
+async function getBackgroundImg(){
+
+    // write code to fetch time from API
+    var response= await fetch ("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
 
  
-  ball = Bodies.circle(50,200,20);
-  World.add(world,ball);
+    //change the data in JSON format and store it in variable responseJSON
+    var responseJson= await response.json();
 
-  slingShot = new Slingshot(this.ball,{x:100,y:200});
+    
+    //fetch datetime from responseJSON
+    var datetime= responseJson.datetime;
 
-}
+    
 
-function draw() {
-  background(56,44,44); 
- 
-  imageMode(CENTER);
-  image(rock_img ,ball.position.x,ball.position.y,40,40);
+    // slice the datetime to extract hour
+    var hour = datetime.slice(11,13);
+console.log(hour);
 
-
-  stroke(0,0,0);
-  fill("white");
-  textSize(20);
-  fill("lightyellow");
-  text("Drag the Hexagonal Stone and Release it, to launch it towards the blocks",100,30);
-
-  ground.display();
-  stand1.display();
-  stand2.display();
-
-  strokeWeight(2);
-  stroke(0,0,0);
-  
-  fill("skyblue");
-  block1.display();
-  block2.display();
-  block3.display();
-  block4.display();
-  block5.display();
-  block6.display();
-  block7.display();
-  fill("pink");
-  block8.display();
-  block9.display();
-  block10.display();
-  block11.display();
-  block12.display();
-  fill("turquoise");
-  block13.display();
-  block14.display();
-  block15.display();
-  fill("grey");
-  block16.display();
-  fill("skyblue");
-  blocks1.display();
-  blocks2.display();
-  blocks3.display();
-  blocks4.display();
-  blocks5.display();
-  fill("turquoise");
-  blocks6.display();
-  blocks7.display();
-  blocks8.display();
-  fill("pink")
-  blocks9.display();
-  fill("gold");
-
-
-  
-  slingShot.display();
-}
-function mouseDragged(){
-  Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
-}
-function mouseReleased(){
-  slingShot.fly();
-}
-
-function keyPressed(){
-  if(keyCode === 32)
-  {
-      slingShot.attach(this.ball);
-  }
+    
+    if(hour>=0 && hour<18 ){
+       bg = "sunrise.png";
+      // bg="sunset.png"
+    }
+    else{
+       bg="sunset.png"
+       //bg = "sunrise.png";
+    }
+    
+    backgroundImg = loadImage(bg);
 }
